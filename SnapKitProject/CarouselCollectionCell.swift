9e0235235dfddb045126c20a185598f653e085ce
Oleg <olegkirsanov22@gmail.com>
2022-04-04 00:04:01 +0300

    Create cell model and carousel view.

--- SnapKitProject/CarouselCollectionCell.swift
+++ SnapKitProject/CarouselCollectionCell.swift
@@ -6,16 +6,45 @@
 //
 
 import UIKit
+import SnapKit
 
 class CarouselCollectionCell: UICollectionViewCell {
     static let identifier = "CarouselCollectionCell"
     
+    private lazy var carouselView: CarouselView = {
+        let frame = CGRect(x: 0, y: 0, width: contentView.frame.width, height: contentView.frame.height)
+        let carouselView = CarouselView(frame: frame)
+        contentView.addSubview(carouselView)
+        carouselView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+        return carouselView
+    }()
+    
+    var cellModel: CellModel? {
+        didSet {
+            carouselView.model = cellModel?.model
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .systemPurple
+        backgroundColor = .systemBackground
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    struct CellModel {
+        var title: String?
+        var description: String?
+        var liked: Bool?
+        var model: CarouselView.CarouselModel?
+    }
+    
+    override func prepareForReuse() {
+        cellModel = nil
+    }
+    
 }
